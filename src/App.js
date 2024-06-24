bf54670a1d7b505991975d8018d0e5d7a4c6879e
sachinseengh <sachinseengh@gmail.com>
2024-06-24 17:07:08 +0545

    custom codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+
+let num=6;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+    <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contacts</li>
+    </nav>
+    <h1>Student number is {num}</h1>
+    <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fugiat aspernatur reiciendis nemo laborum, sit, voluptatibus ipsa mollitia aut vel atque deleniti expedita fuga perspiciatis neque commodi deserunt asperiores est maiores quaerat quas in aperiam veniam! Perspiciatis laudantium nostrum repudiandae!</p>
+    </>
   );
 }
 
