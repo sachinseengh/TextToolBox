5354ece4128691fdf410eddfea02f9b759d25910
sachinseengh <sachinseengh@gmail.com>
2024-06-26 18:10:44 +0545

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 //import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 
 import Navbar from './components/Navbar'
 import TextBox from './components/TextBox'
+// import About from './components/About'
 function App() {
+  const toggleMode=()=>{
+
+    if(mode==="light"){
+      setMode("dark");
+      document.body.style.backgroundColor='#042743';
+
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor='white';
+    }
+  }
+  const [mode,setMode]=useState("light");
   return (
 <>
 
 {/* to pass props  it works like arguments to a function*/}
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container">
-  <TextBox title="Enter you text to analyze"/>
+  <TextBox title="Enter you text to analyze" mode={mode}/>
 </div>
 
+
+{/* <About/> */}
 
 
 
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -20,13 +20,13 @@ export default function TextBox(props){
     }
     const Sc=()=>{
        
-        let newText = text.charAt(0).toUpperCase()+text.substring(1);
+        let newText = text.charAt(0).toUpperCase()+text.substring(1).toLowerCase();
         
         setText(newText);
     }
     const Tc=()=>{
        
-        let newText =text.split(" ").map(word=>word.charAt(0).toUpperCase()+word.substring(1)).join(" ");
+        let newText =text.split(" ").map(word=>word.charAt(0).toUpperCase()+word.substring(1).toLowerCase()).join(" ");
         
         setText(newText);
     }
@@ -34,29 +34,35 @@ export default function TextBox(props){
     
         setText(event.target.value);
     }
+    const handleExtraSpace=()=>{
+        let newtxt = text.split(/[ ]+/);
+        setText(newtxt.join(" "));
+    }
    
     // const [text,setText]=useState("Enter your text here");
     const [text,setText]=useState("");
     return(
         <>
-<div className="container">
+<div className="container" style={{color:props.mode==='light'?'black':'white'}}>
 <div class="mb-3">
-  <h1 className='my-3'>{props.title}</h1>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={Onchange} ></textarea>
+  <h1 className='my-3' >{props.title}</h1>
+  <textarea className="form-control" id="exampleFormControlTextarea1"
+   rows="8"  value={text} onChange={Onchange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='light'?'black':'white'}} ></textarea>
 </div>
 <button className="btn btn-primary mx-2" onClick={Up}>Convert to UpperCase</button>
 <button className="btn btn-primary mx-2 my-1" onClick={Lp}>Convert to LowerCase</button>
 <button className="btn btn-primary mx-2 my-1" onClick={Sc}>Sentence Case</button>
 <button className="btn btn-primary mx-2 my-1" onClick={Tc}>Title Case</button>
+<button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
 </div>
 
-<div className="container my-3">
+<div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
     <h2>Your text Summary</h2>
     <p><b>{text.split(" ").length}</b> Words and <b>{text.length}</b> letters </p> 
 
     <p><b>{0.008* text.split(" ").length}</b> Minutes read</p>
     <h2>Preview</h2>
-    <p>{text}</p>
+    <p>{text.length>0?text:"Enter your text to preview here "}</p>
 </div>
 </>
     )
